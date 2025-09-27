37c52fc56a290ad3f414eb9b8798ddb82d3692aa
oa180 <oa20180180@gmail.com>
2025-09-27 14:47:13 +0300

    c

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: ['https://travelling-pal.vercel.app'],
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://travelling-pal.vercel.app'],
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
